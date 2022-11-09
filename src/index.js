6b6abd489e9c62398c484129995dbcf777eaebc4
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-10 02:09:14 +0500

    remove incomplete extra code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import './style.css';
-import { UI, Todo } from '../modules/index';
+import { UI, Todo } from '../modules/index.js';
 
 // Events
 
 document.addEventListener('DOMContentLoaded', UI.displayTodo);
 
-document.querySelector('.add-to-list').addEventListener('submit', (e) => {
-  const todoText = document.querySelector('.todo-input').value;
-});
+// document.querySelector('.add-to-list').addEventListener('submit', (e) => {
+//    const todoText = document.querySelector('.todo-input').value;
+// });
 
+// eslint-disable-next-line no-undef, no-unused-vars
 const book = new Todo(desc, completed, id);
 
+// eslint-disable-next-line no-undef
 UI.addBookToList(todo);
