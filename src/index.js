025d35e85d64b95d0f0512ee1d3a8bf5f4163034
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-10 18:13:34 +0500

    js clean

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
 import './style.css';
-import { UI, Todo } from '../modules/index.js';
-
-// Events
-
-document.addEventListener('DOMContentLoaded', UI.displayTodo);
-
-// document.querySelector('.add-to-list').addEventListener('submit', (e) => {
-//    const todoText = document.querySelector('.todo-input').value;
-// });
-
-// eslint-disable-next-line no-undef, no-unused-vars
-const book = new Todo(desc, completed, id);
-
-// eslint-disable-next-line no-undef
-UI.addBookToList(todo);
