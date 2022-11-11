785a6922c611b456a4f6c200a932b121334e3409
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-11 09:15:53 +0500

    select Elements in JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 import './style.css';
+// Select the Elements
+const clear = document.querySelector('.clearAll');
+const list = document.querySelector('.js-list');
+const input = document.querySelector('.dataInput');
