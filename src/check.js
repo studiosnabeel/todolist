d0b9b66eddb3dfdd0234488c49671dd652a829d0
Nabeel Akbar <studiosnabeel@gmail.com>
2022-11-12 22:13:04 +0500

    add checkjs file with new code

---
+++ src/check.js
@@ -0,0 +1,22 @@
+export default (toDoTasks) => {
+  const checkBtns = document.querySelectorAll('.checkbox');
+  checkBtns.forEach((checkBtn, index) => {
+    checkBtn.addEventListener('click', () => {
+      checkBtn.childNodes[0].classList.toggle('active');
+      checkBtn.nextElementSibling.classList.toggle('active');
+      if (checkBtn.childNodes[0].classList.contains('active')) {
+        toDoTasks.modifyTask(
+          index,
+          checkBtn.nextElementSibling.textContent.trim(),
+          true
+        );
+      } else {
+        toDoTasks.modifyTask(
+          index,
+          checkBtn.nextElementSibling.textContent.trim(),
+          false
+        );
+      }
+    });
+  });
+};
